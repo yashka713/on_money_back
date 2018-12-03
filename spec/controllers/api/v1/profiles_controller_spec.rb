fd465279ae505862afdc6e6629295114a440813a
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-12-03 11:18:44 +0200

    Added tests for user profile

--- spec/controllers/api/v1/profiles_controller_spec.rb
+++ spec/controllers/api/v1/profiles_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ProfilesController, type: :controller do
+  let!(:user) { create :user }
   let(:valid_user_props) { { user: attributes_for(:user) } }
   let(:invalid_user_props) { { user: { email: 'test@', password: 'Passw1' } } }
 
@@ -46,6 +47,30 @@ RSpec.describe Api::V1::ProfilesController, type: :controller do
           post :registration, params: { user: { email: 'test@test', password: 'passw1' } }
           expect(response.body).to match(/at least one upper case/)
         end
+      end
+    end
+  end
+
+  context 'update:' do
+    let(:name) { 'Test_First_name' }
+    let(:nickname) { 'Test_nickname' }
+    let(:update_params) { { user: { name: name, nickname: nickname } } }
+
+    before { login_user(user) }
+
+    context 'successfully updates user' do
+      before { patch :update, params: update_params }
+
+      it 'and check response code' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'and change name' do
+        (expect { user.reload }).to change { user.name }.from(nil).to(name)
+      end
+
+      it 'and change nickname' do
+        (expect { user.reload }).to change { user.nickname }.from(nil).to(nickname)
       end
     end
   end
