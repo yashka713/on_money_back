acc313191ba9e1f39ee4987246e8c62bb2cb302d
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-12-03 11:18:55 +0200

    Added docs for user profile

--- docs/controllers/api/v1/profiles_controller_docs.rb
+++ docs/controllers/api/v1/profiles_controller_docs.rb
@@ -16,7 +16,7 @@ module Docs
         key :type, :json
         key :required, true
         schema do
-          key :'$ref', :RegistrationUser
+          key :'$ref', :UserCreateCredentials
         end
       end
 
@@ -27,6 +27,68 @@ module Docs
             key :type, :object
             key :'$ref', :ProfileItem
           end
+        end
+      end
+
+      response 422 do
+        key :description, 'Error, when credentials is incorrect'
+        schema do
+          key :type, :object
+          property :errors do
+            key :type, :array
+            items do
+              property :detail, type: :string, example: 'is invalid'
+              property :source, type: :object do
+                property :pointer, type: :string, example: '/data/attributes/email'
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  swagger_path '/api/v1/profiles' do
+    operation :patch do
+      key :description, 'Update a current_user profile'
+      key :summary, 'Update a current_user profile'
+      key :operationId, 'update'
+      key :produces, %w[application/json]
+      key :tags, %w[Profile]
+
+      parameter do
+        key :'$ref', :TokenParam
+        schema do
+          key :'$ref', :TokenExample
+        end
+      end
+
+      parameter do
+        key :name, :user
+        key :in, :body
+        key :description, 'User credentials'
+        key :type, :json
+        key :required, true
+        schema do
+          key :'$ref', :UserUpdateCredentials
+        end
+      end
+
+      response 200 do
+        key :description, 'User was updated'
+        schema do
+          property :data do
+            key :type, :object
+            key :'$ref', :ProfileItem
+          end
+        end
+      end
+
+      response 401 do
+        key :description, 'Error, when credentials is incorrect'
+        schema do
+          key :type, :object
+          key :'$ref', :Unauthorized
         end
       end
 
