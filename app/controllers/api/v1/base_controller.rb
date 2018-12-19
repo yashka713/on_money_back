97e982de737e6e7315fa81c2c87133435ab9d36f
Yaroslav Liakh <liakhyaroslav@gmail.com>
2018-12-19 16:59:49 +0200

    Rescue CanCan Error with render_403

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -7,6 +7,7 @@ module Api
       rescue_from ActiveRecord::RecordNotFound, with: :render_404
       rescue_from ActionController::ParameterMissing, with: :render_400
       rescue_from ArgumentError, with: :render_400
+      rescue_from CanCan::AccessDenied, with: :render_403
 
       before_action :authenticate
     end
