7ccac9fec38688540a4d4930206f51cf691984f1
Yaroslav Liakh <liakhyaroslav@gmail.com>
2019-01-11 14:32:01 +0200

    Fixed currency serializer
    Use currency `id` as `id`, added `iso_code`.

--- app/serializers/currencies_serializer.rb
+++ app/serializers/currencies_serializer.rb
@@ -2,11 +2,12 @@ class CurrenciesSerializer < ActiveModel::Serializer
   attributes :attributes, :type, :id
 
   def read_attribute_for_serialization(args)
-    return @object.iso_code if args == :id
+    return @object.id if args == :id
     return :currency_info if args == :type
 
     {
       name: @object.name,
+      iso_code: @object.iso_code,
       symbol: @object.symbol
     }
   end
