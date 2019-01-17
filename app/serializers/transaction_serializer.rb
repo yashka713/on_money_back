94780a392b4ae5d321a175d043393d1af2c37824
Yaroslav Liakh <liakhyaroslav@gmail.com>
2019-01-17 11:35:55 +0200

    Added Transactions list

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -8,6 +8,7 @@ class TransactionSerializer < ActiveModel::Serializer
 
   attribute :date
   attribute :status
+  attributes :from_symbol, :to_symbol
 
   def date
     @object.date.strftime('%F')
@@ -15,5 +16,17 @@ class TransactionSerializer < ActiveModel::Serializer
 
   def status
     @object.persisted? ? :active : :deleted
+  end
+
+  def from_symbol
+    return Money::Currency.new(@object.chargeable.currency).symbol if @object.chargeable.is_a?(Account)
+
+    Money::Currency.new(@object.profitable.currency).symbol
+  end
+
+  def to_symbol
+    return Money::Currency.new(@object.profitable.currency).symbol if @object.profitable.is_a?(Account)
+
+    Money::Currency.new(@object.chargeable.currency).symbol
   end
 end
