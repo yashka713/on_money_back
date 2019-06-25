2bd47cb0c7d9d287b633ba23b24d0a161fa3dc2d
Yaroslav Liakh <liakhyaroslav@gmail.com>
2019-06-25 18:40:28 +0300

    Stop using SnakeCaseParameters middleware

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module OnMoneyBack
       g.helper          false
     end
 
-    config.middleware.use SnakeCaseParameters
+    # config.middleware.use SnakeCaseParameters
 
     config.middleware.use Rack::Cors do
       allow do
