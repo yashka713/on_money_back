61845a3e1889dc1cf1527a6b831a436ff7db08f7
Yaroslav Liakh <liakhyaroslav@gmail.com>
2020-02-26 20:03:49 +0200

    Delete useless byebug

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 require 'rspec/rails'
-require 'byebug'
 require 'database_cleaner'
 require 'dotenv/load'
 require 'factory_bot'
